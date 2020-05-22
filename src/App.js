22d437738cef98de95903319e4699ce95f2f4764
Alexander Zito-Wolf <azitowolf@Alexanders-MacBook-Pro-2.local>
2020-05-22 18:18:37 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs => {
+      console.log(blogs);
+      setBlogs( blogs )
+    }
+      
+    )  
+  }, [])
+  return (
+    <div>
+      <h2>blogs</h2>
+      {blogs.length ? blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      ) : ''}
+    </div>
+  )
+}
+
+export default App
